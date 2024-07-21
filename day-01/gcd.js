a57adaf24510e5f1f858bbe87f9a13b53e96bc77
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-07-21 08:43:03 +0530

    First commit and coded gcd

---
+++ day-01/gcd.js
@@ -0,0 +1,12 @@
+function gcd(a, b) {
+  while (a !== 0) {
+    let a_ = b % a;
+    let b_ = a;
+    a = a_;
+    b = b_;
+  }
+  return b;
+}
+
+console.log(gcd(10, 18));
+console.log(gcd(20, 28));
